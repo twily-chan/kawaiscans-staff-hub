364c63a8ae477adf9714b939ac7a6c8ec10a854b
twily-chan <raiyaadraad132rafan45@gmail.com>
2026-01-20 17:08:25 +0600

    Fixed Blank Pagre

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -2,10 +2,6 @@
 
 interface ImportMetaEnv {
     readonly BASE_URL: string;
-    readonly MODE: string;
-    readonly DEV: boolean;
-    readonly PROD: boolean;
-    readonly SSR: boolean;
     readonly VITE_GOOGLE_CLIENT_ID: string;
     readonly VITE_ADMIN_EMAILS: string;
     readonly VITE_GITHUB_TOKEN: string;
